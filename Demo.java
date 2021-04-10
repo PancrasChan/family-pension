c008c007b0a471d079ee20fb6c34da3c0d10a82b
PancrasChan <skara_095@163.com>
2021-04-11 02:01:37 +0800

    提交test分支代码

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("这是一个git测试文件");
 		System.out.println("这是第一次修改部分");
-		System.out.println("这是test代码");
+		System.out.println("这是test分支代码");
 	}
 }
